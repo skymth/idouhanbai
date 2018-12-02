f199cf0393eb0b790b9350e7c78d643647517594
soeyu <s1240231@gmail.com>
2018-12-02 13:56:39 +0900

    Create Map.vue

---
+++ idou-project/components/Map.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="map-page">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.7747730850674!2d139.93134191530933!3d37.48964097981246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff55497f9ee833b%3A0x4994f5b084f08535!2z44Of44OL44K544OI44OD44OXIOS8mua0pem2tOODtuWfjuWJjeW6lw!5e0!3m2!1sja!2sjp!4v1543723567707" width="600" height="1050" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+</template>
+<style>
+.map-page {
+  margin-top: 50%;
+  margin-bottom: 60%;
+}
+</style>
